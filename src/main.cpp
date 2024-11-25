284a92b2bf3b74eb5b93b180c32c467b1ddd1e73
ordadush <orssua@gmail.com>
2024-11-25 12:23:12 +0200

    change

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,21 @@
-#include "Simulation.h"
+// #include "Simulation.h"
 #include <iostream>
 
 using namespace std;
 
-Simulation* backup = nullptr;
+// Simulation* backup = nullptr;
 
 int main(int argc, char** argv){
-    cout << "test my run" << endl;
     if(argc!=2){
         cout << "usage: simulation <config_path>" << endl;
         return 0;
     }
     string configurationFile = argv[1];
-    Simulation simulation(configurationFile);
-    simulation.start();
-    if(backup!=nullptr){
-    	delete backup;
-    	backup = nullptr;
-    }
+   // Simulation simulation(configurationFile);
+   // simulation.start();
+   // if(backup!=nullptr){
+   // 	delete backup;
+   // 	backup = nullptr;
+   // }
     return 0;
 }
